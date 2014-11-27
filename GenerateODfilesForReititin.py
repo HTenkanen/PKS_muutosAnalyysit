aeebeaeb28d34acafac93cfaa035b166c1bd4e3c
Henrikki Tenkanen <henrikki.tenkanen@helsinki.fi>
2014-11-27 12:23:59 +0200

    Parse 2017 results and join generate YKR-grids.

--- GenerateODfilesForReititin.py
+++ GenerateODfilesForReititin.py
@@ -1,5 +1,5 @@
 __author__ = 'hentenka'
-#import geopandas as gpd
+import geopandas as gpd
 import pandas as pd
 import numpy as np
 import sys, os
--- ParseReititinResults.py
+++ ParseReititinResults.py
@@ -1,3 +1,58 @@
 __author__ = 'hentenka'
 import pandas as pd
+import os, sys
+import numpy as np
+import geopandas as gpd
+
+# Paths
+dataFolder = r"C:\HY-Data\HENTENKA\MetropAccess-Reititin_1.2\MetropAccess-Reititin\bin\Results"
+population = r"C:\HY-Data\HENTENKA\PKS_saavutettavuusVertailut\YKR_asukkaat2013.shp"
+outputF = r"C:\HY-Data\HENTENKA\PKS_saavutettavuusVertailut\Kauppakeskukset\NopeimmatAjatKauppakeskuksiin"
+
+paths = []
+
+# Parse results text files
+for root, dirs, files in os.walk(dataFolder):
+    for filename in files:
+        if filename.endswith('results.txt'):
+            paths.append(os.path.join(root, filename))
+
+full_data = pd.DataFrame()
+
+# Collect all data to single dataframe
+for result in paths:
+    data = pd.read_csv(result, sep=';')
+
+    # Select columns
+    data = data[['from_id', 'to_id', 'total_route_time', 'route_time', 'route_distance']]
+    full_data = full_data.append(data)
+
+# Change -99999.99 to NaN
+full_data = full_data.replace(to_replace={'total_route_time': {-99999.99: np.nan}})
+
+# Round distance values to full meters
+full_data['PT_dist'] = full_data.apply(lambda x: np.round(x['route_distance']), axis=1)
+
+# Drop NaNs
+full_data = full_data.dropna(axis=0, subset=['total_route_time'])
+
+# Group by destination
+grouped = full_data.groupby('to_id')
+
+# Read ykr-grid and population stats
+pop = gpd.read_file(population)
+
+# Create output names for the results
+outputNames = {5878070: 'Jumbo_2017.shp', 5902043: 'Myyrmanni_2017.shp', 5936704: 'Sello_2017.shp', 5944003: 'Itis_2017.shp', 5975373: 'Kamppi_2017.shp', 5978593: 'Iso_Omena_2017.shp', 5980260: 'Ruoholahti_2017.shp'}
+
+for id, group in grouped:
+
+    # Join Reititin results with YKR-grid (incl. population stats)
+    join = pop.merge(group, how='inter', left_on='YKR_ID', right_on='from_id')
+
+
+
+    print outputNames[id]
+
+
 
