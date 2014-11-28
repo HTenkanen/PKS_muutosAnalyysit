71d3f736ffddba2c7258a41181abadd77b8ff02d
Henrikki Tenkanen <henrikki.tenkanen@helsinki.fi>
2014-11-28 09:38:50 +0200

    Join population projections to YKR-grid

---
+++ Determine_population_projections.py
@@ -0,0 +1,9 @@
+__author__ = 'hentenka'
+import geopandas as gpd
+import pandas as pd
+
+# File paths
+pop_estimates = r"C:\HY-Data\HENTENKA\PKS_saavutettavuusVertailut\VaestoEnnusteet\PKS_Vaestoennusteet2012_2021_Cleaned.xlsx"   # Source: http://www.hri.fi/fi/dataset/paakaupunkiseudun-vaestoennuste-2012-2021
+pks_sub_regions = r"C:\HY-Data\HENTENKA\PKS_saavutettavuusVertailut\Seutukartta\PKS_pienalueet.shp"  # Source: http://www.hri.fi/fi/dataset/seutukartta
+
+
