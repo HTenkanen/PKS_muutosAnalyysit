7683e86b6b55eb70c455b57e3f04a997e068733b
Henrikki Tenkanen <henrikki.tenkanen@helsinki.fi>
2014-12-01 12:29:32 +0200

    Added Population projections for year 2017 associated to YKR-grid.

--- Determine_population_projections.py
+++ Determine_population_projections.py
@@ -1,9 +1,161 @@
 __author__ = 'hentenka'
 import geopandas as gpd
 import pandas as pd
+import sys
+from shapely.ops import cascaded_union, unary_union
+import spatial_tools as st
+import numpy as np
+
+
+def mergePolys(df, attributeDataDictionary, dictionary_row_index):
+
+    pols = []
+    for idx, row in df.iterrows():
+        pols.append(row['geometry'])
+    merge = unary_union(pols)
+    valuesToList = [item[dictionary_row_index] for item in attributeDataDictionary.values()]
+    contents = [[merge] + valuesToList]
+    headers = ['geometry'] + attributeDataDictionary.keys()
+
+    data = gpd.GeoDataFrame(contents, columns=headers, geometry='geometry')
+    return data
+
+def calculatePopEstimates(row, ykr_as, pop2013, pop2017, growth, ykr_count, target):
+    if row[ykr_as] > row[pop2013]:
+        if row[ykr_count] > 0:
+            estim_pop = row[pop2017]/row[ykr_count]
+            if estim_pop > row[ykr_as]:
+                return int(np.round(estim_pop, 0))
+            else:
+                return int(row[ykr_as])
+        else:
+            return int(row[ykr_as])
+    elif int(row[ykr_as]) == 0:
+        return 0
+
+    elif int(row[ykr_as]) == 1:
+        if row[ykr_count] > 0:
+            estim_pop = row[pop2017]/row[ykr_count]
+            if estim_pop > row[ykr_as]:
+                return int(np.round(estim_pop, 0))
+            else:
+                return int(row[ykr_as])
+    else:
+        try:
+            value = row[ykr_as]*row[growth]
+            return int(np.round(value, 0))
+        except:
+            return np.nan
+
 
 # File paths
-pop_estimates = r"C:\HY-Data\HENTENKA\PKS_saavutettavuusVertailut\VaestoEnnusteet\PKS_Vaestoennusteet2012_2021_Cleaned.xlsx"   # Source: http://www.hri.fi/fi/dataset/paakaupunkiseudun-vaestoennuste-2012-2021
+pop_estimates = r"C:\HY-Data\HENTENKA\PKS_saavutettavuusVertailut\VaestoEnnusteet\PKS_Vaestoennusteet2012_2021_Cleaned.csv"   # Source: http://www.hri.fi/fi/dataset/paakaupunkiseudun-vaestoennuste-2012-2021
 pks_sub_regions = r"C:\HY-Data\HENTENKA\PKS_saavutettavuusVertailut\Seutukartta\PKS_pienalueet.shp"  # Source: http://www.hri.fi/fi/dataset/seutukartta
+output = r"C:\HY-Data\HENTENKA\PKS_saavutettavuusVertailut\VaestoEnnusteet\PKS_VaestoEnnusteet_2017.shp"
+ykr_grid = r"C:\HY-Data\HENTENKA\PKS_saavutettavuusVertailut\YKR_asukkaat2013.shp"
 
+pop = pd.read_csv(pop_estimates, sep=';')
+pks = gpd.read_file(pks_sub_regions)
+pks_coordsys = pks.crs
 
+# Fill pop.Mapcode to exact 10 digits long (enabling join with pks)
+pop['RegionCode'] = pop.apply(lambda x: str(x['Mapcode']).zfill(10), axis=1)
+
+# Merge polygons of Kauniainen to a single polygon
+kauniainen = pks.loc[pks['KUNTA'] == '235']
+selectRow = kauniainen[[u'KOKOTUN', u'KUNTA', u'Mtryhm', u'NIMI_ISO', u'Nimi', u'PIEN', u'SUUR', u'TILA']][0:1]
+dict = selectRow.to_dict()
+row_index = selectRow.index.values[0]
+merged_kauniainen = mergePolys(kauniainen, dict, row_index)
+
+# Update KOKOTUN value for Kauniainen
+merged_kauniainen['KOKOTUN'] = '2351000000'
+
+# Remove multiple Kauniainen polygons from original pks data
+pks_subset = pks.loc[pks['KUNTA'] != '235']
+
+# Add merged Kauniainen to pks_subset
+pks_mod = pks_subset.append(merged_kauniainen)
+
+# Join
+join = pks_mod.merge(pop, how='inner', left_on='KOKOTUN', right_on='RegionCode')
+selection = join[[u'KOKOTUN', u'RegionCode', u'Nimi', u'geometry', '2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021']]
+selection.columns = [u'KOKOTUN', u'RegionCode', u'Nimi', u'geometry', 'Pop2000', 'Pop2001', 'Pop2002', 'Pop2003', 'Pop2004', 'Pop2005', 'Pop2006', 'Pop2007', 'Pop2008', 'Pop2009', 'Pop2010', 'Pop2011', 'Pop2012', 'Pop2013', 'Pop2014', 'Pop2015', 'Pop2016', 'Pop2017', 'Pop2018', 'Pop2019', 'Pop2020', 'Pop2021']
+
+# Change . to 0
+selection = selection.replace('.', 1)
+
+# Change datatype to integer
+selection[['Pop2013', 'Pop2017']] = selection[['Pop2013', 'Pop2017']].astype(int)
+
+# Calculate population change (percentage) between years 2013 and 2017
+selection['Pop13vs17'] = selection['Pop2017'] / selection['Pop2013']
+
+pop_data = gpd.GeoDataFrame(selection, geometry='geometry', crs=pks_coordsys)
+
+# Read Population-YKR-2013
+ykr_poly = gpd.read_file(ykr_grid)
+ykr_coordsys = ykr_poly.crs
+
+# Calculate poly centroids from ykr_poly
+ykr_poly['centroid'] = None
+spatial = st.SpatialTools()
+ykr_poly = ykr_poly.apply(spatial.calculateCentroid, axis=1, source_column='geometry', target_column='centroid')
+
+# Determine point as geometry for the dataframe
+ykr_poly.columns = [u'Count_', u'Sum_ASYHT', u'YKR_ID', u'polygon', u'x', u'y', u'geometry']
+ykr_point = ykr_poly  # Rename variable to clarify
+
+# Ensure that datasets are in the same coordinate system
+pop_data_reproj = pop_data.to_crs(crs=ykr_coordsys)
+
+# Create RTree spatial index for population data polygons
+idx = spatial.buildRtree(pop_data_reproj)
+
+# Determine for each ykr_grip (point) the Region Code of pop_estimate dataset
+join_popEstim_ykr = spatial.pointInPolygon(point_df=ykr_point, poly_df=pop_data_reproj, poly_rtree=idx, sourceColumn_in_poly='RegionCode', targetColumn_in_point='RegionCod')
+
+# Select columns from population estimate dataset to the join
+pop_select = pop_data[[u'RegionCode', u'Nimi', u'Pop2000', u'Pop2001', u'Pop2002', u'Pop2003', u'Pop2004', u'Pop2005', u'Pop2006', u'Pop2007', u'Pop2008', u'Pop2009', u'Pop2010', u'Pop2011', u'Pop2012', u'Pop2013', u'Pop2014', u'Pop2015', u'Pop2016', u'Pop2017', u'Pop2018', u'Pop2019', u'Pop2020', u'Pop2021', u'Pop13vs17']]
+
+# Join the datasets based on RegionCode
+ykr_pop_estim = join_popEstim_ykr.merge(pop_select, left_on='RegionCod', right_on='RegionCode', how='inner')
+
+# Select cells that did not have population in 2013
+new_areas = ykr_pop_estim.loc[ykr_pop_estim['Pop2013'] == 1]
+
+# Group by RegionCode
+grouped = new_areas.groupby('RegionCode')
+
+# Create empty data frame
+ykr_count = pd.DataFrame()
+
+# Iterate over groups and calculate how many YKR cells are within new_areas
+for index, group in grouped:
+    code = group['RegionCode'].values[0]
+    count = len(group)
+    ykr_count = ykr_count.append([[code,count]])
+
+ykr_count.reset_index(inplace=True, drop=True)
+ykr_count.columns = ['RegCode', 'Ykr_count']
+
+# Join ykr_counts to data
+ykr_pop_estim = ykr_pop_estim.merge(ykr_count, how='outer', left_on='RegionCode', right_on='RegCode')
+
+# Fill NaN to -1
+ykr_pop_estim.fillna(value=-1, inplace=True)
+
+# Calculate population estimates for year 2017
+ykr_pop_estim['pop_esti17'] = None
+ykr_pop_estim['pop_esti17'] = ykr_pop_estim.apply(calculatePopEstimates, axis=1, ykr_as='Sum_ASYHT', pop2013='Pop2013', pop2017='Pop2017', growth='Pop13vs17', target='pop_estim17', ykr_count='Ykr_count')
+
+# Set geometry back to Polygons
+ykr_pop_estim.columns = [u'Count_', u'Sum_ASYHT', u'YKR_ID', u'geometry', u'x', u'y', u'point', u'RegionCod', u'RegionCode', u'Nimi', u'Pop2000', u'Pop2001', u'Pop2002', u'Pop2003', u'Pop2004', u'Pop2005', u'Pop2006', u'Pop2007', u'Pop2008', u'Pop2009', u'Pop2010', u'Pop2011', u'Pop2012', u'Pop2013', u'Pop2014', u'Pop2015', u'Pop2016', u'Pop2017', u'Pop2018', u'Pop2019', u'Pop2020', u'Pop2021', u'Pop13vs17', u'RegCode', u'Ykr_count', u'pop_esti17']
+
+# Drop point and RegionCod columns
+ykr_pop_estim = ykr_pop_estim.drop(labels=['point', 'RegionCod', 'RegCode'], axis=1)
+
+# Create GeoDataFrame
+geo = gpd.GeoDataFrame(ykr_pop_estim, geometry='geometry', crs=ykr_coordsys)
+
+geo.to_file(output, driver="ESRI Shapefile")
--- spatial_tools.py
+++ spatial_tools.py
@@ -1,119 +1,132 @@
 import numpy as np
-from scipy.spatial import cKDTree
+from scipy.spatial import ckdtree, kdtree
 import pandas as pd
 import geopandas as gpd
 from shapely.geometry import Point, Polygon
 from rtree import index
+import os, sys
 
-def buildRtree(polygon_df):
-    idx = index.Index()
-    for poly in polygon_df.iterrows():
-        idx.insert(poly[0], poly[1]['geometry'].bounds)
-    return idx
+class SpatialTools:
+    def __init__(self):
+        a=None
 
-def querySpatialIndex(point, poly_df, poly_rtree, source_column):
-    """Find poly containing the point"""
-    point_coords = point['geometry'].coords[:][0]
-    for idx_poly in poly_rtree.intersection( point_coords ):
-        if poly_df['geometry'][idx_poly:idx_poly+1].values[0].contains(point['geometry']):
-            return poly_df[source_column][idx_poly:idx_poly+1].values[0]
-    return None
+    def buildRtree(self, polygon_df):
+        idx = index.Index()
+        for poly in polygon_df.iterrows():
+            idx.insert(poly[0], poly[1]['geometry'].bounds)
+        return idx
 
-def pointInPolygon(point_df, poly_df, poly_rtree, sourceColumn_in_poly, targetColumn_in_point, fast_search=True):
-    """Iterates over points"""
-    data = point_df
-    data[targetColumn_in_point] = None
-    data[targetColumn_in_point] = point_df.apply(querySpatialIndex, axis=1, poly_df=poly_df, poly_rtree=poly_rtree, source_column=sourceColumn_in_poly)
-    return data
+    def polygonCentroid(self, poly):
+        return poly.centroid
 
-def parseShapefilePaths(topFolder):
-    paths = []
-    for root, dirs, files in os.walk(topFolder):
-        for filename in files:
-            if filename.endswith('.shp'):
-                paths.append(os.path.join(root,filename))
-    return paths
+    def calculateCentroid(self, row, source_column, target_column):
+        row[target_column] = self.polygonCentroid(row[source_column])
+        return row
 
-def createCoordTuples(data):
-    """Extracts coordinate tuples from Shapely Points objects"""
-    data['xy'] = None
-    for i, row in data.iterrows():
-        data['xy'][i] = [np.round(row['geometry'].x, decimals=5), np.round(row['geometry'].y, decimals=5)]
-    return data
+    def querySpatialIndex(self, point, poly_df, poly_rtree, source_column):
+        """Find poly containing the point"""
+        point_coords = point['geometry'].coords[:][0]
 
-def findNN(from_coords, to_coords):
-    #Search nearest point from 'from_coords'
-    t = cKDTree(list(from_coords['xy']))
+        for idx_poly in poly_rtree.intersection( point_coords ):
+            if poly_df['geometry'][idx_poly:idx_poly+1].values[0].contains(point['geometry']):
+                return poly_df[source_column][idx_poly:idx_poly+1].values[0]
+        return None
 
-    #Extract distance and index of closest point
-    d, idx = t.query(list(to_coords['xy']), k=1) # --> k: number of nearest neighbours that are searched
-    return d,idx
+    def pointInPolygon(self, point_df, poly_df, poly_rtree, sourceColumn_in_poly, targetColumn_in_point):
+        """Iterates over points"""
+        data = point_df
+        data[targetColumn_in_point] = None
+        data[targetColumn_in_point] = point_df.apply(self.querySpatialIndex, axis=1, poly_df=poly_df, poly_rtree=poly_rtree, source_column=sourceColumn_in_poly)
+        return data
 
-def CRS(inputFile):
-    return inputFile.crs
+    def parseShapefilePaths(self, topFolder):
+        paths = []
+        for root, dirs, files in os.walk(topFolder):
+            for filename in files:
+                if filename.endswith('.shp'):
+                    paths.append(os.path.join(root,filename))
+        return paths
 
-def checkCrsMatch(file1, file2):
-    if file1.crs == file2.crs:
-        return True
-    else:
-        return False
+    def createCoordTuples(self, data):
+        """Extracts coordinate tuples from Shapely Points objects"""
+        data['xy'] = None
+        for i, row in data.iterrows():
+            data['xy'][i] = [np.round(row['geometry'].x, decimals=5), np.round(row['geometry'].y, decimals=5)]
+        return data
 
-def spatialJoin(target_df, from_df, keep_all=False, **kwargs):
+    def findNN(self, from_coords, to_coords):
+        #Search nearest point from 'from_coords'
+        t = ckdtree(list(from_coords['xy']))
 
-    #Check that files are in the same coordinate system
-    if not checkCrsMatch(target_df, from_df):
-        raise Exception("Input shapefiles need to be in the same coordinate system!")
+        #Extract distance and index of closest point
+        d, idx = t.query(list(to_coords['xy']), k=1) # --> k: number of nearest neighbours that are searched
+        return d,idx
 
-    #Get columns of 'target_df'
-    origColumns = list(target_df.columns)
+    def CRS(self, inputFile):
+        return inputFile.crs
 
-    #Check attributes
-    for a in kwargs:
-        if a == 'attributes' or a == 'columns' or a == 'attr' or a == 'cols':
-            attributes = kwargs[a]
-            for i, col in enumerate(attributes):
-                if not col in from_df.columns:
-                    e = Exception("Column " + col + " does not exist in join file! Check attributes and correct spelling...\n\tAvailable attributes:" + str(list(from_df.columns)))
-                    raise e
-
-                #If the same attribute name exists also in target file, rename the attribute in join file
-                if col in origColumns:
-                    newname = col + "_2"
-                    del attributes[i]
-                    attributes.append(newname)
+    def checkCrsMatch(self, file1, file2):
+        if file1.crs == file2.crs:
+            return True
         else:
-            raise Exception("Unknown parameter '" + a +"'.")
+            return False
 
-    #Create coordinate tuple column into dataframes
-    target_df = createCoordTuples(target_df)
-    from_df = createCoordTuples(from_df)
+    def spatialJoin(self, target_df, from_df, keep_all=False, **kwargs):
 
-    #Find nearest neighbour's index
-    dist, nnidx = findNN(from_df, target_df)
+        #Check that files are in the same coordinate system
+        if not self.checkCrsMatch(target_df, from_df):
+            raise Exception("Input shapefiles need to be in the same coordinate system!")
 
-    #Create column that has index of the closest point from join_df
-    target_df['nnidx'] = nnidx
+        #Get columns of 'target_df'
+        origColumns = list(target_df.columns)
 
-    #Check join type parameter
-    if keep_all:
-        #joinType = 'outer' #Does not work yet!
-        joinType = 'inner'
-    else:
-        joinType = 'inner'
+        #Check attributes
+        for a in kwargs:
+            if a == 'attributes' or a == 'columns' or a == 'attr' or a == 'cols':
+                attributes = kwargs[a]
+                for i, col in enumerate(attributes):
+                    if not col in from_df.columns:
+                        e = Exception("Column " + col + " does not exist in join file! Check attributes and correct spelling...\n\tAvailable attributes:" + str(list(from_df.columns)))
+                        raise e
 
-    #Join attributes to target_df from join dataframe
-    join = pd.merge(left=target_df, right=from_df, how=joinType, left_on='nnidx', right_index=True, suffixes=('','_2'))
+                    #If the same attribute name exists also in target file, rename the attribute in join file
+                    if col in origColumns:
+                        newname = col + "_2"
+                        del attributes[i]
+                        attributes.append(newname)
+            else:
+                raise Exception("Unknown parameter '" + a +"'.")
 
-    #Drop unnecessary columns from the result (i.e. coordinate tuples, nn-index, geometry the join file
-    join = join.drop(labels=['xy','xy_2', 'nnidx', 'geometry_2'], axis=1)
+        #Create coordinate tuple column into dataframes
+        target_df = self.createCoordTuples(target_df)
+        from_df = self.createCoordTuples(from_df)
 
-    #Check attributes parameter --> User can choose what attributes will be taken from the join dataframe
-    if len(kwargs) == 0:
-        return join
-    else:
-        wantedColumns = origColumns + attributes
-        join = join[wantedColumns]
-        return join
+        #Find nearest neighbour's index
+        dist, nnidx = self.findNN(from_df, target_df)
+
+        #Create column that has index of the closest point from join_df
+        target_df['nnidx'] = nnidx
+
+        #Check join type parameter
+        if keep_all:
+            #joinType = 'outer' #Does not work yet!
+            joinType = 'inner'
+        else:
+            joinType = 'inner'
+
+        #Join attributes to target_df from join dataframe
+        join = pd.merge(left=target_df, right=from_df, how=joinType, left_on='nnidx', right_index=True, suffixes=('','_2'))
+
+        #Drop unnecessary columns from the result (i.e. coordinate tuples, nn-index, geometry the join file
+        join = join.drop(labels=['xy','xy_2', 'nnidx', 'geometry_2'], axis=1)
+
+        #Check attributes parameter --> User can choose what attributes will be taken from the join dataframe
+        if len(kwargs) == 0:
+            return join
+        else:
+            wantedColumns = origColumns + attributes
+            join = join[wantedColumns]
+            return join
 
 def main():
     target = r"...\Target_file.shp"
